7d7449e3714de9939a63035c31a2506c4cd30ca5
MCatherine <Mcatherine1994@gmail.com>
2018-02-23 14:15:28 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var myGamePiece;
 
 function startGame() {
-    myGameArea.start();
     myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+        this.canvas.width = 480;
+        this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -30,6 +30,7 @@ function component(width, height, color, x, y) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+
 function updateGameArea() {
     myGameArea.clear();
     myGamePiece.update();
