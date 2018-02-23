1c569698970e1c9ee41827718ec7653f4f15b465
MCatherine <Mcatherine1994@gmail.com>
2018-02-23 13:44:22 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
+var myGamePiece;
+
+
 function startGame() {
     myGameArea.start();
+    myGamePiece = new component("red", 20);
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 }
+
+function component(color, size) {
+    this.size = size;
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(20, 120, size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
