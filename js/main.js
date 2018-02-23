a17fcb0431d2d914ec1d94a4258e642e58ebe9c6
MCatherine <Mcatherine1994@gmail.com>
2018-02-23 14:11:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var myGamePiece;
 
-
 function startGame() {
     myGameArea.start();
     myGamePiece = new component("red", 20);
@@ -13,15 +12,24 @@ var myGameArea = {
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 function component(color, size) {
     this.size = size;
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(20, 120, size, 0, 2 * Math.PI);
-    ctx.fill();
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(20, 120, size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 }
 
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.update();
+}
