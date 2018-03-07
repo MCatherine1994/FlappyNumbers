6efd415fda0447af57e14ac1f06208eb39d45977
MCatherine <Mcatherine1994@gmail.com>
2018-03-07 09:52:47 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,20 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
+        //set the gaming area to the whole window
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        //run the updateGameArea every 20th millisecond (50 times per second)
         this.interval = setInterval(updateGameArea, 20);
+        //check if the a key is pressed, and set the key property of the myGameArea to the key code. When the key is released, set teh key property to false
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -20,19 +29,32 @@ var myGameArea = {
 }
 
 function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;    
-     this.update = function(){
+    this.update = function(){
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function(){
+        this.x +=this.speedX;
+        this.y = this.speedY;
     }
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.x += 1;
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;    
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();
     myGamePiece.update();
 }
