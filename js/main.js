433ad5cccf2f4f5bdd0f619bbc45ca2eda14c63b
MCatherine <Mcatherine1994@gmail.com>
2018-03-15 15:31:59 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var myGameArea = {
 }
 
 function startGame(){
-    myGamePiece = new component(80, 80, "img/test.png", 300, 300, "image");
+    myGamePiece = new component(80, 80, "img/fish0.png", 300, 300, "image");
     myBackground = new component(window.innerWidth, window.innerHeight, "img/bg3.jpg", 0, 0, "background");
     myGameArea.start();
     //myObstacle = new component(20, 20, "green", 300, 120);
@@ -100,7 +100,11 @@ function updateGameArea(){
     if(myGameArea.frameNo == 1 || everyinterval(150)){
         x = myGameArea.canvas.width;
         y = myGameArea.canvas.height - 300;
-        myObstacles.push(new component(100, 100, "img/test.png", x, y, "image"));
+        var z = num.toString(Math.floor((Math.random() * 5) + 1));
+        var str1 = "img/fish";
+        var str2 = str1.concat(z);
+        var str3 = str2.concat(".png")
+        myObstacles.push(new component(100, 100, str3, x, y, "image"));
         /*minHeight = 20;
         maxHeight = 200;
         height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
